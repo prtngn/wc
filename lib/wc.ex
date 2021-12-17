dd20842f7da5587b90d72c27f96cf99c6c9eb723
Maxim Portnyagin <maxim@prtngn.cc>
2021-12-17 10:16:33 +0300

    Streaming

--- lib/wc.ex
+++ lib/wc.ex
@@ -1,9 +1,17 @@
-defmodule Wc do
-  def count do
-    {:ok, file} = File.read("book.txt")
-    file
-    |> String.split(~r{[\r\n\[\]()\s.,]}, trim: true)
-    |> Enum.count
+defmodule WC do
+  def count(filename) do
+    stream = File.stream!(filename)
+    |> Stream.map(&String.split(&1, ~r{\s}, trim: true))
+    |> Stream.map(&Enum.count/1)
+
+    words = Enum.sum(stream)
+    IO.puts("Words in file: #{words}")
   end
 end
 
+defmodule WC.CLI do
+  def main(args) do
+    filename = hd(args)
+    WC.count(filename)
+  end
+end
